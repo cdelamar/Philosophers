a9bc99bb7b17bed611990093c46b724bdddbfb54
cdelamar <contact.clementdelamare@gmail.com>
2024-05-23 09:04:19 +0200

    correcting segfault issue

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cdelamar <cdelamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 10:29:57 by cdelamar          #+#    #+#             */
-/*   Updated: 2024/05/23 03:18:14 by cdelamar         ###   ########.fr       */
+/*   Updated: 2024/05/23 09:00:01 by cdelamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@
 
 bool nobody_died (t_philo *philo)
 {
-	unsigned int i;
+	//unsigned int i;
 
-	i = 0;
-	while (i < philo->data->philo_nb)
-	{
-		printf (">>>> L >>>> philo[%d].data->eat_time = %lu\n", i, philo[i].data->eat_time);
-		printf (">    R    > philo[%d].last_eat_time = %lu\n", i, philo[i]. last_eat_time);
-		if (philo[i].last_eat_time > philo[i].data->eat_time)
-			return (false);
-		i++;
-	}
+	//i = 0;
+	//while (i < philo->data->philo_nb)
+	//{
+		//printf (">>>> L >>>> philo[%d].data->eat_time = %lu\n", i, philo[i].data->eat_time);
+		//printf (">    R    > philo[%d].last_eat_time = %lu\n", i, philo[i]. last_eat_time);
+	if (philo->last_eat_time > philo->data->eat_time)
+		return (false);
+	//	i++;
+	//}
 	return (true);
 }
 
